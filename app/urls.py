9fcad426eed71b22a4b966681180ebb082d4e1d8
scllovely <Shachenlu_lucky@163.com>
2025-04-22 01:18:59 +0800

    Changes-新增三方信息的录入和展示

--- app/urls.py
+++ app/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     path('educationLogs/<str:module>/', app.views.EducationLogsView.as_view()),
     path('projectLogs/<str:module>/', app.views.ProjectLogsView.as_view()),
     path('sendLogs/<str:module>/', app.views.SendLogs.as_view()),
-
     path('tripartite_info/<path:module>/', app.views.TripartiteInfoView.as_view()),
 ]
